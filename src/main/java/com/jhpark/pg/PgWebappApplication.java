07d549cffe28276951cfbd5d626f33e978d52b7a
jhpark-mac <ggoowlgns@gmail.com>
2023-03-05 21:49:03 +0900

    init from ide

---
+++ src/main/java/com/jhpark/pg/PgWebappApplication.java
@@ -0,0 +1,13 @@
+package com.jhpark.pg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgWebappApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PgWebappApplication.class, args);
+  }
+
+}
